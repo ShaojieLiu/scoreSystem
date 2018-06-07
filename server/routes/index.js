7c6d4722676ad681f47daf7fe2f05026d13133fa
刘少杰 <439655668@qq.com>
2018-06-07 17:43:39 +0800

    乱七八糟

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  // res.sendFile(__dirName + '../build/index.html')
+  res.json('123')
 });
 
 module.exports = router;
