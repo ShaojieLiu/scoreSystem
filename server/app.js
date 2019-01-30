43b515607d2d72beb432d394f48fa3f0883a3899
刘少杰 <439655668@qq.com>
2019-01-30 15:47:39 +0800

    改IP

--- server/app.js
+++ server/app.js
@@ -1,33 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-const proxy = require('http-proxy-middleware');
-var {publicPath} = require('./env')
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const proxy = require("http-proxy-middleware");
+var { publicPath } = require("./env");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static(publicPath));
 
-
-
-app.use('/users', usersRouter);
-app.use(proxy('/score', { target: 'http://localhost:7001' }))
-app.use(proxy('/config', { target: 'http://localhost:7001' }))
-app.use(proxy('/', { target: 'http://localhost:3001' })) // 完全代理了devserver
+app.use("/users", usersRouter);
+app.use(proxy("/score", { target: "http://172.18.90.234:7001" }));
+app.use(proxy("/config", { target: "http://172.18.90.234:7001" }));
+app.use(proxy("/", { target: "http://172.18.90.234:3001" })); // 完全代理了devserver
 
 // app.use('/', indexRouter);
 
@@ -40,11 +38,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
